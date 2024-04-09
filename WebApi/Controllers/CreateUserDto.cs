502bdd8661d7aa99e6452d84c76ec1a591e0b272
barto <barto@LAPTOP-DELL>
2024-04-09 14:25:17 +0200

    Add project files.

---
+++ WebApi/Controllers/CreateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Controllers
+{
+    public class CreateUserDto
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ WebApi/Controllers/FreightExchangeController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using WebApi.Controllers.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FreightExchangeController : BaseApiController
+    {
+
+        private static List<User> _users = new List<User>()
+        {
+            new Entities.User()
+            {
+                Id = 1,
+                Name = "Test",
+                Email = "Test"
+            },
+            new Entities.User()
+            {
+                Id = 2,
+                Name = "Test2",
+                Email = "Test2"
+            }
+        };
+
+        private static int _idCount = _users.Count + 1;
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return _users;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            User? us = _users.FirstOrDefault(user => user.Id == id);
+            if (us == null)
+            {
+                return BadRequest();
+            }
+            return Ok(us);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUserById(int id)
+        {
+            User? user = _users.FirstOrDefault(user => user.Id == id);
+            if (user == null)
+            {
+                return BadRequest();
+            }
+            if (_users.Remove(user))
+            {
+                return Ok();
+            }
+            return Ok();//TODO what if item is not succesfully removed
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser(CreateUserDto dto)
+        {
+            User newUser = new User() { Id = _idCount++, Name = dto.Name, Email = dto.Email };
+            _users.Add(newUser);
+            return Created($"/users/{newUser.Id}", newUser);
+        }
+
+        //[HttpPut]
+        //public IActionResult OverwriteUser()
+        //{
+
+        //}
+
+    }
+}
