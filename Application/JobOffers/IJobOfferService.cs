5ebb57938219f1ac97be4d6000fb1e40a626c359
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-11 13:13:53 +0200

    Added information about fatal errors that can occur while using IJobOfferService and IUserService

--- Application/JobOffers/IJobOfferService.cs
+++ Application/JobOffers/IJobOfferService.cs
@@ -11,9 +11,31 @@ namespace Application.JobOffers
     public interface IJobOfferService
     {
         IEnumerable<JobOfferDto> GetAll();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jobOffer"></param>
+        /// <exception cref="JobOffers.JobOfferNotFoundException">Method throws JobOfferNotFoundException when can't find specified jobOffer</exception>
         JobOfferDto GetById(int id);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jobOffer"></param>
+        /// <exception cref="JobOffers.JobOfferNotFoundException">Method throws JobOfferNotFoundException when can't find specified jobOffer</exception>
+        /// <exception cref="JobOffers.JobOfferNotDeletedException">Method throws JobOfferNotDeletedException when can't delete specified jobOffer</exception>
         void Delete(int id);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jobOffer"></param>
+        /// <exception cref="JobOffers.JobOfferNotCreatedException">Method throws JobOfferNotCreatedException when can't create specified jobOffer</exception>
         int Create(JobOfferDto jobOfferDto);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jobOffer"></param>
+        /// <exception cref="JobOffers.JobOfferNotFoundException">Method throws JobOfferNotFoundException when can't find specified jobOffer</exception>
+        /// <exception cref="JobOffers.JobOfferNotUpdatedException">Method throws JobOfferNotUpdatedException when can't update specified jobOffer</exception>
         JobOfferDto Update(JobOfferDto jobOfferDto);
     }
 }
--- Application/Users/IUserService.cs
+++ Application/Users/IUserService.cs
@@ -10,9 +10,31 @@ namespace Application.Users
     public interface IUserService
     {
         IEnumerable<UserDto> GetAll();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <exception cref="Users.UserNotFoundException">Method throws UserNotFoundException when can't find specified user</exception>
         UserDto GetById(int id);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <exception cref="Users.UserNotFoundException">Method throws UserNotFoundException when can't find specified user</exception>
+        /// <exception cref="Users.UserNotDeletedException">Method throws UserNotDeletedException when can't delete specified user</exception>
         void Delete(int id);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <exception cref="Users.UserNotCreatedException">Method throws UserNotCreatedException when can't create specified user</exception>
         int Create(UserDto user);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <exception cref="Users.UserNotFoundException">Method throws UserNotFoundException when can't find specified user</exception>
+        /// <exception cref="Users.UserNotUpdatedException">Method throws UserNotUpdatedException when can't update specified user</exception>
         UserDto Update(UserDto user);
     }
 }
