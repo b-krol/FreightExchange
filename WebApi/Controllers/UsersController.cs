b0fbed45a48747d77ade6145cb454ebe4fb85000
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-11 11:22:28 +0200

    Deleted empty not-needed BaseApisController class

--- WebApi/Controllers/UsersController.cs
+++ WebApi/Controllers/UsersController.cs
@@ -10,7 +10,7 @@ namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController : BaseApiController
+    public class UsersController : ControllerBase
     {
         private IUserService UserService { get; }
 
