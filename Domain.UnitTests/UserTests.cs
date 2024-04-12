a38a4b15f70e840de8caf60eb76ad669a50cce9e
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-12 14:46:13 +0200

    Added series of unit test to User constructor

---
+++ Domain.UnitTests/UserTests.cs
@@ -0,0 +1,115 @@
+using Domain.User;
+
+namespace Domain.UnitTests
+{
+    [TestFixture]
+    public class UserTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithNullName()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User(null!, "xyz@domain.com"));
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithEmptyName()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User(string.Empty, "xyz@domain.com"));
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithNameMadeOfSpacesOnly()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User("    ", "xyz@domain.com"));
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithNullEmail()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User("Name", null!));
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithEmptyEmail()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User("Name", String.Empty));
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithEmailMadeOfSpacesOnly()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User("Name", "        "));
+        }
+
+        [Test]
+        public void UserCannotBeCreatedWithoutContainingMonkeySymbolInEmail()
+        {
+            Assert.Throws<ArgumentException>(() => new User.User("Name", "xyz#domain.com"));
+        }
+
+        //[Test]
+        //public void UserCannotBeCreatedWithoutContainingDotInEmail()
+        //{
+        //    Assert.Throws<ArgumentException>(() => new User.User("Name", "xyz@domain,com"));
+        //}
+
+        //[Test]
+        //public void UserCannotBeCreatedWithoutContainingDotAfterMonkeySymbolInEmail()
+        //{
+        //    Assert.Throws<ArgumentException>(() => new User.User("Name", "xyz.domain@com"));
+        //}
+
+        //[Test]
+        //public void UserCannotBeCreatedWithoutContainingDotAfterMonkeySymbolWithAtLeastOneCharacterBetweenThemInEmail()
+        //{
+        //    Assert.Throws<ArgumentException>(() => new User.User("Name", "xyz@.com"));
+        //}
+
+        //[Test]
+        //public void UserCannotBeCreatedWithoutContainingDotAfterMonkeySymbolWithAtLeastOneCharacterNotBeingSpaceBetweenThemInEmail()
+        //{
+        //    Assert.Throws<ArgumentException>(() => new User.User("Name", "xyz@  .com"));
+        //}
+
+        //[Test]
+        //public void UserCannotBeCreatedWithoutContainingDotAfterMonkeySymbolWithAtLeastOneCharacterBeingLetterBetweenThemInEmail()
+        //{
+        //    throw new NotImplementedException("Test not implemented yet");
+        //}
+
+        //[Test]
+        //public void UserCannotBeCreatedWithContainingAnySpaceInEmail()
+        //{
+        //    Assert.Throws<ArgumentException>(() => new User.User("Name", "xyz @domain.com"));
+        //}
+
+        [Test]
+        public void UserCanBeCreatedWithCorrectData()
+        {
+            var createdUser = new User.User("Normal name", "xyz@domain.com");
+            Assert.That(createdUser.Name, Is.SameAs("Normal name"));
+            Assert.That(createdUser.Email, Is.SameAs("xyz@domain.com"));
+        }
+
+        [Test]
+        public void UserCanBeCreatedWithCorrectDataWithNameContainingWhiteSpacesInConstructorAndWithoutThemInNameProperty()
+        {
+            var createdUser = new User.User("  Normal name ", "xyz@domain.com");
+            Assert.That(createdUser.Name, Is.SameAs("Normal name"));
+            Assert.That(createdUser.Email, Is.SameAs("xyz@domain.com"));
+        }
+
+        [Test]
+        public void UserCanBeCreatedWithCorrectDataWithEmailContainingWhiteSpacesInConstructorAndWithoutThemInEmailProperty()
+        {
+            var createdUser = new User.User("Normal name", "  xyz@domain.com ");
+            Assert.That(createdUser.Name, Is.SameAs("Normal name"));
+            Assert.That(createdUser.Email, Is.SameAs("xyz@domain.com"));
+        }
+    }
+}
