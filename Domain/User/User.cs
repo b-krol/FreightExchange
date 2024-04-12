5cc71a8708f314f8c85ee8ac08f81d60bda56bef
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-12 14:45:40 +0200

    Added User class constructor !Warning! not all validation methods are implemented yet

--- Domain/User/User.cs
+++ Domain/User/User.cs
@@ -1,9 +1,33 @@
-﻿namespace Domain.User
+﻿using System;
+
+namespace Domain.User
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public User(string name, string email)
+        {
+
+            ThrowIfNullOrEmpty(name);
+            ThrowIfNullOrEmpty(email);
+            ThrowIfEmailIsIncorrect(email);
+
+            Name = name;
+            Email = email;
+        }
+
+        private void ThrowIfEmailIsIncorrect(string email)
+        {
+            throw new NotImplementedException();//TODO implement email validation
+        }
+
+        private void ThrowIfNullOrEmpty(string value)
+        {
+            if (value == null) throw new ArgumentException();
+            if (value == string.Empty) throw new ArgumentException();
+            if (value.Trim() == string.Empty) throw new ArgumentException();
+        }
     }
 }
