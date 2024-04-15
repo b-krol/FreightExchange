d4da5d9a33b0a8aa0161d4813d07c4147cf0c581
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-15 13:35:46 +0200

    Added BaseDomainException

---
+++ Domain/BaseDomainException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class BaseDomainException : Exception
+    {
+        private const string BasicExceptionMessage = "Domain exception";
+        public BaseDomainException(string message) : base(message)
+        {
+        }
+
+        public BaseDomainException(string message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public BaseDomainException() : base(BasicExceptionMessage)
+        {
+        }
+    }
+}
