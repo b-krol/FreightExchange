9f77199631b69110ee15c2d07387b7533418e63e
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-15 13:36:51 +0200

    Added CartageErrandExecutionStatusChangeNotAllowedException inheriting from BaseDomainException

---
+++ Domain/CartageErrand/CartageErrandExecutionStatusChangeNotAllowedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.CartageErrand
+{
+    public class CartageErrandExecutionStatusChangeNotAllowedException : BaseDomainException
+    {
+        private const string BasicExceptionMessage = "Cannot change CartageErrand ExecutionStatus";
+        public CartageErrandExecutionStatusChangeNotAllowedException(string message) : base(message)
+        {
+        }
+
+        public CartageErrandExecutionStatusChangeNotAllowedException(string message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public CartageErrandExecutionStatusChangeNotAllowedException() : base(BasicExceptionMessage)
+        {
+        }
+    }
+}
