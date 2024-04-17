9c1253207b6a147801469c190881836e4247b4c2
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-17 09:57:30 +0200

    Added ICartageOfferService interface

---
+++ Application/CartageOffers/ICartageOfferService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CartageOffers
+{
+    public interface ICartageOfferService
+    {
+        IEnumerable<CartageOfferDto> GetAll();
+        CartageOfferDto GetById(int id);
+        void Delete(int id);
+        int Create(CartageOfferDto cartageoffer);
+        CartageOfferDto Update(CartageOfferDto cartageoffer);
+    }
+}
