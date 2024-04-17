3de2d77bd5c3ba99fa5aa97154020a571ebcd47c
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-17 09:54:56 +0200

    Added CartageOfferDto

---
+++ Application/CartageOffers/CartageOfferDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CartageOffers
+{
+    public class CartageOfferDto
+    {
+        public int? Id { get; init; }
+        public int BidderId { get; init; }
+        public int Price { get; init; }
+        public bool? HasBeenConsidered { get; init; }
+        public bool? HasBeenAccepted { get; init; }
+    }
+}
