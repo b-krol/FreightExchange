71164400fede3e2527f90347b3b9794c993ba3c1
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-17 10:00:50 +0200

    Added CartageOfferService, not implemented yet

---
+++ Application/CartageOffers/CartageOfferService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CartageOffers
+{
+    internal class CartageOfferService : ICartageOfferService
+    {
+        private readonly IDataSource Source;
+        public CartageOfferService(IDataSource source)
+        {
+            Source = source;
+        }
+        public int Create(CartageOfferDto cartageoffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CartageOfferDto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartageOfferDto GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CartageOfferDto Update(CartageOfferDto cartageoffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
