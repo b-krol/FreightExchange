420cb4e15f0089abc9814ca0a65f58cc1b379c4e
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-18 13:23:22 +0200

    Added CartageOfferNotFoundException exception

---
+++ Application/CartageOffers/CartageOfferNotFoundException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CartageOffers
+{
+    public class CartageOfferNotFoundException : BaseApplicationException
+    {
+        private const string BasicExceptionMessage = "CartageOffer not found";
+        public CartageOfferNotFoundException(string message) : base(message)
+        {
+        }
+
+        public CartageOfferNotFoundException(string message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public CartageOfferNotFoundException() : base(BasicExceptionMessage)
+        {
+        }
+    }
+}
