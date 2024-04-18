fd1c44214a034c37fdf5c36e4101b58ed00b808b
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-18 13:26:26 +0200

    Added CartageOfferNotDeletedException exception

---
+++ Application/CartageOffers/CartageOfferNotDeletedException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.CartageOffers
+{
+    public class CartageOfferNotDeletedException : BaseApplicationException
+    {
+        private const string BasicExceptionMessage = "CartageOffer couldn't get deleted";
+        public CartageOfferNotDeletedException(string message) : base(message)
+        {
+        }
+
+        public CartageOfferNotDeletedException(string message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        public CartageOfferNotDeletedException() : base(BasicExceptionMessage)
+        {
+        }
+    }
+}
