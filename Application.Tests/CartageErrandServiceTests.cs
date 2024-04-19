a4018b34b450fcba6c36ccd33c0920764066a2f4
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-19 13:39:25 +0200

    Added 2 tests verifying if returned DTO contains and contains matching to the asked CartageErrand id in CartageErrandServiceTests

--- Application.Tests/CartageErrandServiceTests.cs
+++ Application.Tests/CartageErrandServiceTests.cs
@@ -3,6 +3,7 @@ using Application.Users;
 using Domain.CartageErrand;
 using Domain.User;
 using NSubstitute;
+using NUnit.Framework.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,28 @@ namespace Application.UnitTests
     [TestFixture]
     public class CartageErrandServiceTests
     {
+        private Randomizer Rand = Randomizer.CreateRandomizer();
+
+        #region staticMethods
+        private static User CreateCorrectUser(int id)
+        {
+            return new User("Bidder", "mrBidd3r@domain.com") { Id = id };
+        }
+
+        private static CartageErrand CreateCorrectCartageErrand(int id)
+        {
+            return new CartageErrand(
+                CreateCorrectUser(id),
+                "planks",
+                "Radom ul. Słowackiego 3",
+                "Gdańsk",
+                500,
+                13.4f,
+                3000,
+                DateTime.Now + new TimeSpan(24, 0, 0)
+                )
+            { Id = id };
+        }
         private static CartageErrandDto CreateCartageErrandDto(int id)
         {
             return new CartageErrandDto()
@@ -28,6 +51,8 @@ namespace Application.UnitTests
                 EndDate = DateTime.Now + new TimeSpan(24, 0, 0)
             };
         }
+        #endregion
+
         [Test]
         public void CartageErrandCannotBeAddedWithFounderIdOfNotExistingUser()
         {
@@ -39,6 +64,36 @@ namespace Application.UnitTests
 
             Assert.ThrowsAsync<UserNotFoundException>(async () => await service.Add(cartageErrandDto));
             dataSource.Received(1).GetUserById(cartageErrandDto.FounderId);
+        }
+
+        [Test]
+        public void CartageErrandServiceHasToReturnIdOfCreatedCartageErrand()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ReturnedCartageErrandRepresentingObjectHasToContainSetId()
+        {
+            int cartageErrandId = Rand.Next(2, 2000);
+
+            var dataSource = Substitute.For<IDataSource>();
+            dataSource.GetCartageErrandById(Arg.Any<int>()).Returns(Task.FromResult(CreateCorrectCartageErrand(cartageErrandId)));
+            var service = new CartageErrandService(dataSource);
+
+            Assert.That(dataSource.GetCartageErrandById(cartageErrandId).Result.Id, !Is.Null);
+        }
+
+        [Test]
+        public void ReturnedCartageErrandRepresentingObjectHasToContainMatchingSetId()
+        {
+            int cartageErrandId = Rand.Next(2, 2000);
+
+            var dataSource = Substitute.For<IDataSource>();
+            dataSource.GetCartageErrandById(Arg.Any<int>()).Returns(Task.FromResult(CreateCorrectCartageErrand(cartageErrandId)));
+            var service = new CartageErrandService(dataSource);
+
+            Assert.That(dataSource.GetCartageErrandById(cartageErrandId).Result.Id, Is.EqualTo(cartageErrandId));
         }
     }
 }
