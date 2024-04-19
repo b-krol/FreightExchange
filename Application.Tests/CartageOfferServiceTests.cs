b880f7cfb20dd9e7164205bd5164ac2a1bf1e2ec
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-19 09:29:21 +0200

    Added not implemented yet CartageOffersServiceTests

---
+++ Application.Tests/CartageOfferServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests
+{
+    [TestFixture]
+    public class CartageOfferServiceTests
+    {
+        [Test]
+        public void CartageOfferCannotBeAddedToNotExistingCartageErrand()
+        {
+
+        }
+
+        [Test]
+        public void CartageOfferCannotBeAddedWithBidderIdOfNotExistingUser()
+        {
+                
+        }
+
+        [Test]
+        public void CartageOfferHasToBeAddedToSpecifiedCartageErrand()
+        {
+
+        }
+    }
+}
