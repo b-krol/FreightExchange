63f592cbe54597e27e62c68fc49cf85de498176f
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-22 14:33:41 +0200

    Added DataSourceContext

---
+++ Persistence/DataSourceContext.cs
@@ -0,0 +1,28 @@
+﻿using Domain.CartageErrand;
+using Domain.CartageOffer;
+using Domain.User;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class DataSourceContext : DbContext//TODO has to implement IDataSource interface
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<CartageErrand> CartageErrands { get; set; }
+        public DbSet<CartageOffer> CartageOffers { get; set; }
+
+        public DataSourceContext(DbContextOptions<DataSourceContext> options) : base(options)
+        { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+    }
+}
