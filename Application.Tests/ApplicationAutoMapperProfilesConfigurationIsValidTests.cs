dd682fc4a0e17b20e670c1c1182eafd2c504714e
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-22 10:37:42 +0200

    Added ApplicationAutoMapperProfilesConfigurationIsValidTests to test newly added map Profile

---
+++ Application.Tests/ApplicationAutoMapperProfilesConfigurationIsValidTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.MapProfile;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UnitTests
+{
+    [TestFixture]
+    public class ApplicationAutoMapperProfilesConfigurationIsValidTests
+    {
+        private static IEnumerable<MapperConfiguration> TestCases()
+        {
+            yield return CreateConfiguration(
+              new EntityToDtoMap());
+        }
+
+        [TestCaseSource($"{nameof(TestCases)}")]
+        public void ConfigurationTest(MapperConfiguration configuration)
+        {
+            IMapper mapper = configuration.CreateMapper();
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+
+        protected static MapperConfiguration CreateConfiguration(params Profile[] profiles)
+        {
+            return new MapperConfiguration((Action<IMapperConfigurationExpression>)(x => x.AddProfiles((IEnumerable<Profile>)profiles)));
+        }
+    }
+}
