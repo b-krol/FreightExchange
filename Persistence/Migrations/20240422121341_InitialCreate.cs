95499bc2c29a70c0a5f8979ce5906f9363cc4368
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-22 14:35:25 +0200

    Launched first database's migration

---
+++ Persistence/Migrations/20240422121341_InitialCreate.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartageErrands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FounderId = table.Column<int>(type: "int", nullable: false),
+                    GoodsName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StartingAdress = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    DestinationAdress = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Distance = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<float>(type: "real", nullable: false),
+                    MaximumPrice = table.Column<int>(type: "int", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExecutionStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartageErrands", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartageErrands_Users_FounderId",
+                        column: x => x.FounderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartageOffers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BidderId = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    ConsiderationStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartageOffers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartageOffers_Users_BidderId",
+                        column: x => x.BidderId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartageErrands_FounderId",
+                table: "CartageErrands",
+                column: "FounderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartageOffers_BidderId",
+                table: "CartageOffers",
+                column: "BidderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartageErrands");
+
+            migrationBuilder.DropTable(
+                name: "CartageOffers");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
