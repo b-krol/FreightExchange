15c89bf4d1ffa422f222cdef1b66cec20b5f7b6f
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-22 10:35:11 +0200

    Implemented EntityToDtoMap map Profile to cast domain objects to their dto's

--- Application/MapProfile/EntityToDtoMap.cs
+++ Application/MapProfile/EntityToDtoMap.cs
@@ -1,7 +1,9 @@
 ﻿using Application.CartageErrands;
+using Application.CartageOffers;
 using Application.Users;
 using AutoMapper;
 using Domain.CartageErrand;
+using Domain.CartageOffer;
 using Domain.User;
 using System;
 using System.Collections.Generic;
@@ -16,7 +18,14 @@ namespace Application.MapProfile
         public EntityToDtoMap() 
         {
             CreateMap<User, UserDto>();
-            CreateMap<CartageErrand, CartageErrandDto>();
+            CreateMap<CartageErrand, CartageErrandDto>()
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.ExecutionStatus == CartageErrandExecutionStatus.Active));
+            CreateMap<CartageErrand, CartageErrandWithOffersDto>()
+                .ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => src.ExecutionStatus == CartageErrandExecutionStatus.Active))
+                .ForMember(dest => dest.Offers, opt => opt.MapFrom(src => src.GetSubmittedCartageOffers()));
+            CreateMap<CartageOffer, CartageOfferDto>()
+                .ForMember(dest => dest.HasBeenConsidered, opt => opt.MapFrom(src => src.ConsiderationStatus != CartageOfferConsiderationStatus.Waiting))
+                .ForMember(dest => dest.HasBeenAccepted, opt => opt.MapFrom(src => src.ConsiderationStatus == CartageOfferConsiderationStatus.Accepted));
         }
     }
 }
