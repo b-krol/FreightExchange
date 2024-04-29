9a8ab2876575803a76a4d7fb1d3fba8ff7d2a96d
Bartosz Król <bartosz.krol777@gmail.com>
2024-04-29 08:55:34 +0200

    Added database migration to make Users table contain password

--- Persistence/Migrations/20240429064157_AddedPasswordRecordInUser.Designer.cs
+++ Persistence/Migrations/20240429064157_AddedPasswordRecordInUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
@@ -11,9 +12,11 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataSourceContext))]
-    partial class DataSourceContextModelSnapshot : ModelSnapshot
+    [Migration("20240429064157_AddedPasswordRecordInUser")]
+    partial class AddedPasswordRecordInUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
